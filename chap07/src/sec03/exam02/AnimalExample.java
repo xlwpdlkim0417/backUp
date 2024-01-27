d1ae85e0626ad622271bbc56ef7537447f1f97ae
xlwpdlkim0417 <xlwpdlkim0417@gmail.com>
2024-01-27 17:41:57 +0900

    0127

--- chap07/src/sec03/exam02/AnimalExample.java
+++ chap07/src/sec03/exam02/AnimalExample.java
@@ -26,22 +26,33 @@ public class AnimalExample {
 		// Animal 클래스의 객체를 참조하는 변수 animal에
 		// new 연산자와 Dog 클래스 생성자로 인스턴스 생성 후 메모리 주소 저장
 		// 변수 animal이 Dog 인스턴스를 참조
+
+		// ★ 이 과정을 통해 Dog 인스턴스가 Animal 타입으로 업캐스팅
+
 		animal.sound();
 		// animal.로 Dog 클래스의 재정의 된 추상 메소드 호출
 		animal = new Cat();
 		// Animal 클래스의 객체를 참조하는 변수 animal에
 		// new 연산자와 Dog 클래스 생성자로 인스턴스 생성 후 메모리 주소 저장
 		// 변수 animal이 Cat 인스턴스를 참조
+
+		// ★ 이 과정을 통해 Cat 인스턴스가 Animal 타입으로 업캐스팅
+
 		animal.sound();
-		// animal.로 Dog 클래스의 재정의 된 추상 메소드 호출
+		// animal.로 Cat 클래스의 재정의 된 추상 메소드 호출
 		System.out.println("────────────────────");
 
 		// 메소드의 다형성
 		animalSound(new Dog());
-		// 추상 클래스 Animal의 추상 메소드 호출 ▷
+		// AnimalExample 클래스의 정적 메소드 호출
 		// 매개 변수 : new Dog()
 		// Dog 클래스에서 재정의 된 추상 메소드 호출
+		// 이 인스턴스는 메소드 내에서 Animal 타입으로 처리
 		animalSound(new Cat());
+		// AnimalExample 클래스의 정적 메소드 호출
+		// 매개 변수 : new Dog()
+		// Dog 클래스에서 재정의 된 추상 메소드 호출
+		// 이 인스턴스는 메소드 내에서 Animal 타입으로 처리
 
 	}
 
@@ -51,6 +62,7 @@ public class AnimalExample {
 		// 매개 변수 : Animal animal
 		animal.sound();
 		// 추상 클래스 Animal의 추상 메소드 호출
+		// 매개변수로 받은 Animal 타입 객체의 sound 메소드 호출
+		// 실제 실행되는 메소드는 매개변수로 전달된 객체의 클래스에 따라 결정됨 (동적 바인딩)
 	}
-
 }
--- chap08/src/sec01/exam05/SmartTelevisionExample.java
+++ chap08/src/sec01/exam05/SmartTelevisionExample.java
@@ -7,14 +7,18 @@ public class SmartTelevisionExample {
 
 	public static void main(String[] args) {
 		SmartTelevision tv = new SmartTelevision();
-		// [RemoteControl, Searchable] 인터페이스를 다중 구현한 SmartTelevision 클래스의 참조 변수 tv에
-		// SmartTelevision() 생성자를 통해서 새로운 객체 생성 (클래스 컴파일 할 때 별도의 생성자없다면 기본 생성자 자동 생성)
+		// RemoteControl, Searchable 인터페이스를 다중 구현한 SmartTelevision 클래스의 객체를 참조하는 변수 tv
+		// 선언
+		// SmartTelevision() 생성자로 인스턴스 생성 후 메모리 주소 저장
 
 		RemoteControl rc = tv; // 이 부분 모양이 좀 납득이 안가네 다른데서 이렇게 쓴 적이 있던가?
-		// 인터페이스 RemoteControl의 참조 변수 rc에 참조 변수 tv에 담겨있는 내용을 저장
+		// RemoteControl 인터페이스의 구현 객체를 참조하는 변수 rc에 변수 tv 저장
 		// 결국 이 형태도 왼쪽이 부모이고 오른쪽이 자식이라고 생각하면 됨
+		// RemoteControl rc = new SmartTelevision(); 이런 느낌인 셈
+		// RemoteControl 인터페이스의 구현 객체 생성 X
+		// 이미 생성된 SmartTelevision 객체 재사용 ▷ 구현 객체
 
 		Searchable searchable = tv;
+		// Searchable 인터페이스의 구현 객체 생성
 	}
-
 }
